ee81d744e0379fb61e6505efe18d09e2a2ae07c4
Michael Cu <michael.sy.cu@gmail.com>
2020-03-10 18:04:37 -0500

    Added .NET/SQL Server

---
+++ Server/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CommerceBank.transaction" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+    <title>Commerce Bank - Transaction</title>
+        
+        <link rel="stylesheet" type="text/css" href="CSS/StyleSheet.css"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <br />
+            <asp:GridView ID="GridView1" AlternatingRowStyle-CssClass="Grid" runat="server"></asp:GridView>
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Server/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CommerceBank
+{
+    public partial class transaction : System.Web.UI.Page
+    {
+
+        //Connects SQL to the file directory
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+
+
+        //SQL Connection on Page Load
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Connection is open, close the current connection.
+            if(con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            //Open new connection
+            con.Open();
+
+            //Display the Data Table
+            disp_data();
+        }
+
+        //Displays the current table
+        public void disp_data()
+        {
+            //Creates a command for the SQL query
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from CustomerData";
+
+            //Executes the SQL query
+            cmd.ExecuteNonQuery();
+
+            //Data Table Read from sql server
+            DataTable dt = new DataTable();
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            //Fills the SQL Data by the Data Table
+            da.Fill(dt);
+
+            //Gets the Data from the data table and places it into the HTML
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
